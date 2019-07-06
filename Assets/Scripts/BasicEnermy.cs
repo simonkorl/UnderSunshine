a02b3a636bb23ff5921d67c708ed52d4832b0baa
simonkorl <machuan0228@sina.com>
2019-07-06 15:51:36 +0800

    Add MoverController and BasicEnermy class
    The MoveController class can mvoe the object it attaches to.

---
+++ Assets/Scripts/BasicEnermy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnermy : MonoBehaviour
+{
+	public float viewRange;
+	public float viewHeight;
+	private float viewDirect;
+	public Transform[] movePointset;
+	public bool controlled;
+	public MoveController beController;
+    // Start is called before the first frame update
+    void Start()
+    {
+		beController = GetComponent<MoveController>();
+		MoveController playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<MoveController>();
+		beController.Copy(playerController);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controlled)
+		{
+			beController.Move();
+		}
+    }
+}

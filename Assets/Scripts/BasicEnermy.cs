6146f38b424aadd7339b47b916b52d25700932c9
simonkorl <machuan0228@sina.com>
2019-07-06 17:09:30 +0800

    the BasicEnermy is now compilable

--- Assets/Scripts/BasicEnermy.cs
+++ Assets/Scripts/BasicEnermy.cs
@@ -6,8 +6,12 @@ public class BasicEnermy : MonoBehaviour
 {
 	public float viewRange;
 	public float viewHeight;
-	private float viewDirect;
-	public Transform[] movePointset;
+	public Vector2 leftPatrolPoint;
+	public Vector2 rightPatrolPoint;
+	public bool movingRight;
+	public float stayTime;
+	private float staytimer;
+	private bool staying;
 	public bool controlled;
 	public MoveController beController;
     // Start is called before the first frame update
@@ -19,9 +23,53 @@ public class BasicEnermy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+		// 如果被控制，则玩家操控
         if(controlled)
 		{
-			beController.Move();
+			beController.ControlMove();
+			// 如果有可以交互的道具
+			// 则可以交互
+		}
+		else
+		{ // 如果不被控制，则自己行动
+			if(staying)
+			{
+				if(staytimer >= stayTime)
+				{
+					transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localPosition.z);
+				}
+				else
+				{
+					staytimer += Time.deltaTime;
+					return;
+				}
+			}
+
+			if(transform.localScale.x > 0)
+			{
+				if(transform.position.x < rightPatrolPoint.x)
+				{
+					beController.MoveRight();
+				}
+				else
+				{
+					staying = true;
+					beController.Stop();
+				}
+			}
+			else
+			{
+				if(transform.position.x > leftPatrolPoint.x)
+				{
+					beController.MoveLeft();
+				}
+				else
+				{
+					staying = true;
+					beController.Stop();
+				}
+			}
 		}
     }
 }

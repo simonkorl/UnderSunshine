5c14aad7d452a93ee5fa2802a8c6fc3bdd1a47b6
simonkorl <machuan0228@sina.com>
2019-07-07 02:01:26 +0800

    disable an enemy from being controlled twice+

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,13 @@ public class GameManager : MonoBehaviour {
 
 	public Player Player;
 	public GameObject currentObject;
-
-	//public static GameManager instance;
+	public UnityEngine.UI.Slider test_Slider; //! only for test
+	// public static GameManager instance;
 
 	void Awake()
 	{
 		Player = FindObjectOfType<Player>();
+		currentObject = Player.gameObject;
 		Player.controlled = true;
 
 		MoveController[] moveObjects = FindObjectsOfType<MoveController>(); 
@@ -24,17 +25,31 @@ public class GameManager : MonoBehaviour {
 
 	public void setControll(GameObject current)
 	{
+		//? we might need to add camera tracking when we change the control
 		setControllable(currentObject, false);
 		currentObject = current;
 		setControllable(currentObject, true);
 	}
-
+	//! only for test
+	public void showSkillBar(float ratio) 
+	{
+		//TODO make a real skill progress bar
+		test_Slider.value = ratio;
+	}
 	void setControllable(GameObject current, bool controllable)
 	{
 		if(current.name == Player.gameObject.name)
 			current.GetComponent<Player>().controlled = controllable;
 		else
+		{
 			current.GetComponent<BasicEnemy>().controlled = controllable;
+			if(controllable == false)
+			{
+				current.GetComponent<MoveController>().canMove = false;
+				current.GetComponent<BasicEnemy>().canBeControlled = false;
+			}
+		}
+			
 	}
 
 	// Use this for initialization

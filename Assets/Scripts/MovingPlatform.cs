17955e29c5a5da02f56cfca0bb31d11aa76dee2b
煌瞳小妖 <892611452@qq.com>
2019-07-07 11:12:44 +0800

    移动平台

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float movingDistance;
+    public float m_MovingTime;
+    Vector3 originPos;
+    Vector3 targetPos;
+
+    public void startMoving()
+    {
+        StartCoroutine(moving());
+    }
+
+    IEnumerator moving()
+    {
+        transform.position = originPos;
+        float currentTime = Time.time;
+        while(Time.time - currentTime <= m_MovingTime)
+        {
+            transform.position = Vector3.Lerp(originPos, targetPos, (Time.time - currentTime)/m_MovingTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return null;
+        transform.position = targetPos;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originPos = transform.position;
+        targetPos = new Vector3(originPos.x + movingDistance, originPos.y, originPos.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            startMoving();
+        }
+    }
+}

923a3032440f96df37703df43e3121e316b9f705
王天懿 <wty16@mails.tsinghua.edu.cn>
2019-07-07 14:11:13 +0800

    制作部分ppt“

---
+++ Assets/Scripts/PPT.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PPT : MonoBehaviour
+{
+    public SpriteRenderer[] cgs;
+    public string nextLevel;
+    float duration = 1f;
+    int current = 0;
+
+    IEnumerator Play()
+    {
+    	for(int i = 0; i < cgs.Length; i++)
+    	{
+    		while(!Input.anyKeyDown)
+	        	yield return 0;
+	        SpriteRenderer cg = cgs[i];
+	        for(float t = 0; t < duration; t += Time.deltaTime)
+	        {
+	        	cg.color = new Color(1.0f, 1.0f, 1.0f, 1 - t / duration);
+	        	yield return 0;
+	        }
+	        cg.color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+    	}
+    	SceneManager.LoadScene(nextLevel);
+    }
+
+    void Start()
+    {
+    	StartCoroutine(Play());
+    }
+}

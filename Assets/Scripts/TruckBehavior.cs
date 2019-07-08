b101b7baa0667f2395a5f0313e2941c17eb0610f
Shiqing <shiqing-thu18@yandex.com>
2019-07-08 17:02:41 +0800

    Add SFX on the truck

--- Assets/Scripts/TruckBehavior.cs
+++ Assets/Scripts/TruckBehavior.cs
@@ -13,6 +13,12 @@ public class TruckBehavior : MonoBehaviour
 	private SpriteRenderer sRenderer;
 	private GameObject player;
 	bool operated = false;
+
+	public AudioSource audioCarStart;
+	public AudioSource audioCarRunning;
+	public AudioSource audioCarStop;
+	protected float totalTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,6 +43,13 @@ public class TruckBehavior : MonoBehaviour
 	private void FixedUpdate() {
 		findTarget();
 	}
+	private float LinearEnvelope(float t, float t1, float t2, float v1, float v2)
+	{
+		// Mathf.Clamp does not work since v1 may be greater than v2
+		if (t <= t1) return v1;
+		if (t >= t2) return v2;
+		return (t - t1) / (t2 - t1) * (v2 - v1) + v1;
+	}
     // Update is called once per frame
     void Update()
     {
@@ -46,8 +59,29 @@ public class TruckBehavior : MonoBehaviour
 			player.GetComponent<PlayerSkill>().takeBackControll();
 			Destroy(driver);
 			// change the image
-			truckMove.startMoving();
 			operated = true;
+			totalTime = 0;
+			audioCarStart.volume = 1;
+			audioCarStart.Play();
+			audioCarRunning.Play();
+		}
+		if (operated)
+		{
+			float totalTimePrev = totalTime;
+			totalTime += Time.deltaTime;
+			audioCarRunning.volume =    // (/_ ;)
+				totalTime < 4 ?
+				LinearEnvelope(totalTime, 1.5f, 1.6f, 0, 0.8f) :
+				LinearEnvelope(totalTime, 6.6f, 6.8f, 0.8f, 0f);
+			if (totalTimePrev < 1.6f && totalTime >= 1.6f)
+			{
+				truckMove.startMoving();
+			}
+			else if (totalTimePrev < 6.6f && totalTime >= 6.6f)
+			{
+				audioCarStop.volume = 1;
+				audioCarStop.Play();
+			}
 		}
     }
 }
